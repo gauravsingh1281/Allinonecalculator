28d9c80d54be0ffb282a70f2f00e869fe31cd90a
gauravsingh1281 <85905403+gauravsingh1281@users.noreply.github.com>
2022-06-02 23:23:47 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let a;
+let date;
+let time;
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+setInterval(() => {
+    a = new Date(); 
+    date = a.toLocaleDateString(undefined, options);
+    time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
+    document.getElementById('time').innerHTML = " <b>Date</b> &ndash; " + date + " <b>Time</b> &ndash; " + time;}, 1000);
